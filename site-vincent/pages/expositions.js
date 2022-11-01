366f42f085b332c8ecdaa9a929decc72d96994f1
Elkjdsvn <quentinmorali@gmail.com>
2022-11-02 00:35:53 +0100

    new Expo page

---
+++ site-vincent/pages/expositions.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Expositions.module.css'
+
+export default function Expositions() {
+    return (
+        <main className={styles.container}>
+            <h1>Expositions</h1>
+            <p>Projet d’exposition d’UP North prévu pour 2023.</p>
+        </main>
+    )
+}
